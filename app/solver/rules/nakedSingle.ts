a6aa17d5339f0223f174bd52257a7ae8adc01641
Alex Taker <a.taker@gmail.com>
2025-04-24 17:25:51 -0400

    write nakedSingle rule

--- app/solver/rules/nakedSingle.ts
+++ app/solver/rules/nakedSingle.ts
@@ -1,5 +1,15 @@
 // if only one possibily remains for a square, that is the value
 
-export function nakedSingle() {
-  
+export function nakedSingle(vals:any[]) {
+  // find values that are an array of length 1
+  const single = vals.filter((val) => {
+    return Array.isArray(val) && val.length === 1;
+  });
+  if (single.length) {
+    // always want to change the first one
+    const updateIndex = vals.indexOf(single[0]);
+    vals[updateIndex] = single[0][0];
+    return vals;
+  }
+  return false;
 }
