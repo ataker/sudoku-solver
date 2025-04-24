8e0b4c1ff36dfa8354f757783d59375902ed65f6
Alex Taker <a.taker@gmail.com>
2025-04-24 18:38:59 -0400

    write hiddenSingle rule

--- app/solver/rules/hiddenSingle.ts
+++ app/solver/rules/hiddenSingle.ts
@@ -1,6 +1,23 @@
+import { arrayUnique, indexOfArrayOfArrayHasValue } from "../utils/arrayUtils";
+
 // if a possible value is unique in its row/column/house, that is the value
 
-
-export function hiddenSingle() {
+export function hiddenSingle(vals:any[]) {
   
+  const arrs = vals.filter((val) => {
+    return Array.isArray(val);
+  });
+  
+  let uniqueVals = arrayUnique(...arrs);
+  if (uniqueVals.length) {
+    let uniqueVal = uniqueVals[0];
+    const squareToUpdate = indexOfArrayOfArrayHasValue(uniqueVal, ...arrs);
+    
+     vals[vals.indexOf(arrs[squareToUpdate])] = uniqueVal;
+     return vals;
+  }
+  
+  return false;
 }
+
+
