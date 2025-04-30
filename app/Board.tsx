c773f8b6807979f62ba8da47054a839239408d07
Alex Taker <a.taker@gmail.com>
2025-04-29 21:20:59 -0400

    cleanup

--- app/Board.tsx
+++ app/Board.tsx
@@ -2,12 +2,12 @@ import Row from "./Row";
 import { Square } from "./Square";
 import { SudokuState } from "./SudokuState";
 
-export default function Board (sudokuState:SudokuState) {
+export default function Board ({sudokuState}:{sudokuState:SudokuState}) {
   
   return (
     <>
       {
-        sudokuState.sudokuState.getState().map((row) => {
+        sudokuState.getState().map((row) => {
           let vals = row.map((square:Square) => {
             return square.getValue()
           })
